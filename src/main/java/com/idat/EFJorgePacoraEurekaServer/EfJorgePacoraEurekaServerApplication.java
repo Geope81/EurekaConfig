344354a0432e2c1274c2fc14cf37833024d8bd94
Geope81 <geope.2981@gmail.com>
2022-08-18 23:42:55 -0500

    ultimo

---
+++ src/main/java/com/idat/EFJorgePacoraEurekaServer/EfJorgePacoraEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.idat.EFJorgePacoraEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EfJorgePacoraEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfJorgePacoraEurekaServerApplication.class, args);
+	}
+
+}
